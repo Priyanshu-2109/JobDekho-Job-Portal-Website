132cfd176e0c901bcb55b46050f3f5f5e305eec6
Priyanshu Chaniyara <piyuthehero@gmail.com>
2025-02-18 19:39:32 +0530

    Add job listing feature with job card component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -9,6 +9,16 @@ const Hero = () => {
     const {setSearchFilter, setIsSearched} = useContext(AppContext);
 
     const titleRef = useRef(null);
+    const locationRef = useRef(null);
+
+    const onSearch = () => {
+        setSearchFilter({
+            title: titleRef.current.value,
+            location: locationRef.current.value
+        });
+        setIsSearched(true);
+
+    }
 
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
@@ -18,13 +28,13 @@ const Hero = () => {
         <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto '>
             <div className='flex items-center '>
                 <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
-                <input type="text" placeholder='Search for jobs' className='max-sm text-xs p-2 rounded outline-none w-full' />
+                <input type="text" placeholder='Search for jobs' className='max-sm text-xs p-2 rounded outline-none w-full' ref={titleRef} />
             </div>
             <div className='flex items-center '>
                 <img className='h-4 sm:h-5' src={assets.location_icon} alt="" />
-                <input type="text" placeholder='Location' className='max-sm text-xs p-2 rounded outline-none w-full' />
+                <input type="text" placeholder='Location' className='max-sm text-xs p-2 rounded outline-none w-full' ref={locationRef} />
             </div>
-            <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
+            <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
         </div>
       </div>
       <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,22 +1,37 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) =>
 {
-    const [searchfilter, setSearchFilter] = useState({
+    const [searchFilter, setSearchFilter] = useState({
         title: '',
         location: '',
     });
 
     const[isSearched, setIsSearched] = useState(false);
 
+    const[jobs,setJobs] = useState([]);
+
+    //Function to fetch jobs
+    const fetchJobs = async () => {
+        setJobs(jobsData)
+    }
+
+    useEffect(() => {
+        fetchJobs();
+    },[]);
+
+
     const value = {
-        searchfilter,
+        searchFilter,
         setSearchFilter,
         isSearched, 
         setIsSearched,
+        jobs,
+        setJobs
     };
 
     return(
